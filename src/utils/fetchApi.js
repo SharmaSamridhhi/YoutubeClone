c820e02ab09d5d794fa69d284853ee05854f5ebc
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2024-10-05 09:57:09 +0530

    Completed Project

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Base_URL = "https://www.googleapis.com/youtube/v3";
-const API_KEY = "AIzaSyDZpqSIcpEC0YOc6z-_BMjKyPALQh3n5yg";
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const fetchApiForYoutubeData = async (
   endpoints,
